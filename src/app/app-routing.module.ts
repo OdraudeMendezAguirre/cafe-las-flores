17ab8b0713bef2b82aeedc2c04717a5129426122
devllacroix <ayrtonbolanos.diseno@gmail.com>
2022-11-24 15:49:38 -0600

    Avance Enlace Nodos Odry

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginaBienvenidoComponent } from './pagina-bienvenido/pagina-bienvenido.component';
+import { PaginaCarritoComponent } from './pagina-carrito/pagina-carrito.component';
+import { PaginaContactoComponent } from './pagina-contacto/pagina-contacto.component';
+import { PaginaIndexComponent } from './pagina-index/pagina-index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: PaginaIndexComponent },
+  { path: 'bienvenido', component: PaginaBienvenidoComponent },
+  { path: 'carrito', component: PaginaCarritoComponent },
+  { path: 'contacto', component: PaginaContactoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
